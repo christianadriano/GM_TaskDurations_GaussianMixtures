7e9d8893b73b4e5adbdf01ebbcc0aa36b2c85f1b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-24 21:49:06 +0200

    Create EM_TestDuration_E2.R

---
+++ EM_TestDuration_E2.R
@@ -0,0 +1,19 @@
+"
+Gaussian Mixture Model for the testDuration in E2
+"
+
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+library(tidyr)
+library(tidyverse)
+
+#scripts to run EM algorithm
+source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//main_bivariate_EM.R")
+source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//prior_kmeans_EM.R")
+
+#Load data from demographics and qualification test Experiment-2
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
+df_E2_ground<- df_consent
+
+df_prior <- prior.df(faithful = df_consent$testDuration_minutes)
