7cb30bfb2864ee8e9fb9cd3399619ea0225d1d25
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-24 21:05:40 +0200

    Create m_step_ExpectationMaximization.R

---
+++ m_step_ExpectationMaximization.R
@@ -0,0 +1,26 @@
+#source: https://tinyheero.github.io/2016/01/03/gmm-em.html
+#' Maximization Step of the EM Algorithm
+#'
+#' Update the Component Parameters
+#'
+#' @param x Input data.
+#' @param posterior.df Posterior probability data.frame.
+#' @return Named list containing the mean (mu), variance (var), and mixing
+#'   weights (alpha) for each component.
+m_step <- function(x, posterior.df) {
+  comp1.n <- sum(posterior.df[, 1])
+  comp2.n <- sum(posterior.df[, 2])
+  
+  comp1.mu <- 1/comp1.n * sum(posterior.df[, 1] * x)
+  comp2.mu <- 1/comp2.n * sum(posterior.df[, 2] * x)
+  
+  comp1.var <- sum(posterior.df[, 1] * (x - comp1.mu)^2) * 1/comp1.n
+  comp2.var <- sum(posterior.df[, 2] * (x - comp2.mu)^2) * 1/comp2.n
+  
+  comp1.alpha <- comp1.n / length(x)
+  comp2.alpha <- comp2.n / length(x)
+  
+  list("mu" = c(comp1.mu, comp2.mu),
+       "var" = c(comp1.var, comp2.var),
+       "alpha" = c(comp1.alpha, comp2.alpha))
+}
