b4b817696778bf5b291b201f0a5556a9f25e7920
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-10-24 21:49:10 +0200

    Update main_bivariate_EM.R

--- main_bivariate_EM.R
+++ main_bivariate_EM.R
@@ -3,38 +3,35 @@ Gaussian Mixture model for the test duration.
 
 "
 
-library(ggplot2)
-library(dplyr)
-library(hrbrthemes)
-library(tidyr)
-library(tidyverse)
 
-#Load only Consent data. No data from tasks, only from demographics and qualification test
-source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
-df_E2_ground<- df_consent
+source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//e_step_ExpectationMaximization.R")
+source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//m_step_ExpectationMaximization.R")
 
 
 
-for (i in 1:50) {
-  if (i == 1) {
-    # Initialization
-    e.step <- e_step(wait, wait.summary.df[["mu"]], wait.summary.df[["std"]],
-                     wait.summary.df[["alpha"]])
-    m.step <- m_step(wait, e.step[["posterior.df"]])
-    cur.loglik <- e.step[["loglik"]]
-    loglik.vector <- e.step[["loglik"]]
-  } else {
-    # Repeat E and M steps till convergence
-    e.step <- e_step(wait, m.step[["mu"]], sqrt(m.step[["var"]]), 
-                     m.step[["alpha"]])
-    m.step <- m_step(wait, e.step[["posterior.df"]])
-    loglik.vector <- c(loglik.vector, e.step[["loglik"]])
-    
-    loglik.diff <- abs((cur.loglik - e.step[["loglik"]]))
-    if(loglik.diff < 1e-6) {
-      break
-    } else {
+main <- function(wait) {
+  
+  for (i in 1:50) {
+    if (i == 1) {
+      # Initialization
+      e.step <- e_step(wait, wait.summary.df[["mu"]], wait.summary.df[["std"]],
+                       wait.summary.df[["alpha"]])
+      m.step <- m_step(wait, e.step[["posterior.df"]])
       cur.loglik <- e.step[["loglik"]]
+      loglik.vector <- e.step[["loglik"]]
+    } else {
+      # Repeat E and M steps till convergence
+      e.step <- e_step(wait, m.step[["mu"]], sqrt(m.step[["var"]]), 
+                       m.step[["alpha"]])
+      m.step <- m_step(wait, e.step[["posterior.df"]])
+      loglik.vector <- c(loglik.vector, e.step[["loglik"]])
+      
+      loglik.diff <- abs((cur.loglik - e.step[["loglik"]]))
+      if(loglik.diff < 1e-6) {
+        break
+      } else {
+        cur.loglik <- e.step[["loglik"]]
+      }
     }
   }
 }
