3e56efa6a4f9d1bacbe827cf9c9aa58586a17839
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-12-28 23:07:46 +0100

    Update EM_TestDuration_E2.R

--- EM_TestDuration_E2.R
+++ EM_TestDuration_E2.R
@@ -11,78 +11,8 @@ library(tidyverse)
 #scripts to run EM algorithm
 source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//util//main_bivariate_EM.R")
 source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//util//prior_kmeans_EM.R")
+source("C://Users//Christian//Documents//GitHub//EM_GaussianMixtureModel_TaskDurations//util//visualize_compute_membership.R")
 
-
-#------------------
-#Visualize results
-#' Plot a Mixture Component
-#' 
-#' @param x Input ata.
-#' @param mu Mean of component.
-#' @param sigma Standard of component.
-#' @param lam Mixture weight of component.
-plot_mix_comps <- function(x, mu, sigma, lam) {
-  lam * dnorm(x, mu, sigma)
-}
-
-#' @param wait Input ata.
-#' @param m.step approximated mixture models produced by the EM algorithm
-plot_mixture_models <- function(wait,m.step,title_prefix){ 
-
-    if(m.step$mu[1]>m.step$mu[2]){
-      color_1 <- "red"
-      color_2 <- "darkblue"
-    }else{
-      color_1 <- "darkblue"
-      color_2 <- "red"
-    }
-  
-    plot <- data.frame(x = wait) %>%
-    ggplot() +
-    geom_histogram(aes(x, ..density..), binwidth = 0.5, colour = "black", 
-                   fill = "lightblue") +
-    theme_minimal()+
-    stat_function(geom = "line", fun = plot_mix_comps,
-                  args = list(m.step$mu[1], sqrt(m.step$var[1]), 
-                              lam = m.step$alpha[1]),
-                  colour = color_1, lwd = 1.0) +
-    stat_function(geom = "line", fun = plot_mix_comps,
-                  args = list(m.step$mu[2], sqrt(m.step$var[2]), 
-                              lam = m.step$alpha[2]),
-                  colour = color_2, lwd = 1.0) +
-    ylab("Density") +
-    xlab("Test Duration (minutes), binwidth=0.5") +
-    ggtitle(paste0(title_prefix,": Gaussian Mixture Model for Test Duration"))
-}
-
-
-    
-#---------------------------------------------------------------------
-#Compute cluster membership probability
-compute_Memberships <- function(mstep,df){
-  
-  datapoints <- df$test_duration
-  mu_1 <-  m.step$mu[1]
-  mu_2 <- m.step$mu[2]
-  var_1 <- m.step$var[1]
-  var_2 <- m.step$var[2]
-  
-  #probability of seeing datapoints if comming from Gaussian 1
-  p_1_vector <- pnorm(datapoints,mu_1,var_1)
-  p_2_vector <- pnorm(datapoints,mu_2,var_2)
-  
-  p_1_membership <- p_1_vector / (p_1_vector + p_2_vector)
-  p_2_membership <- p_2_vector / (p_1_vector + p_2_vector)
-  
-  if(mu_1>mu_2){
-    df$testDuration_slowMembership <- p_1_membership
-    df$testDuration_fastMembership <- p_2_membership
-  }else{
-    df$testDuration_slowMembership <- p_2_membership
-    df$testDuration_fastMembership <- p_1_membership
-  }
-  return(df)
-}
 
 #---------------------------------------------------------
 #----------------------------------
@@ -198,7 +128,7 @@ The data is more balanced for all,except Professionals, who none fit in two Gaus
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
 #Now compute the membership by each profession, 
-#because professions have different mean values for testDuration.
+#because professions have different mean values for test_duration.
 
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
 
