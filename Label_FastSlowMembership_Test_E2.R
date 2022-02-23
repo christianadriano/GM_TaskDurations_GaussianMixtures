37e445edee06a68c8e4c51123680145fa5f4b5df
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-02-23 17:29:23 +0100

    Create Label_FastSlowMembership_Test_E2.R

---
+++ Label_FastSlowMembership_Test_E2.R
@@ -0,0 +1,14 @@
+"
+Label participant test results according to the speed of their answers,
+which was determined by the participation on the category 
+Fast Answers Membership Probability provided in the field 
+testDuration_fastMembership (between 0 and 1)
+
+Categorization can be based on the mean or median. The assumption for using the
+mean is that it is representative if the distribution is Gaussian. However,
+if the distribution is skewed, which it is (
+check script Distribution_FastSlowMembership_Test_E2.R)
+
+"
+
+
