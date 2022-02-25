ba25f2357d5158643c6f98e5cd7c9d13e2513432
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-02-25 23:59:48 +0100

    Update TestBimodality_TestDuration_E2.R

--- TestBimodality_TestDuration_E2.R
+++ TestBimodality_TestDuration_E2.R
@@ -17,8 +17,6 @@ prepareData <- function(){
 
 df_consent <- prepareData()
 
-professions <- unique(df_consent$profession)
-
 df_selected <-
   dplyr::select(df_consent,
                 profession,
@@ -26,8 +24,15 @@ df_selected <-
                 progr_years,
                 age_years,
                 test_duration,
+                qualification_score,
                 test_score #outcome variable
   );
+
+
+#-------------------------------------------
+# TEST ACROSS PROFESSIONS
+
+professions <- unique(df_consent$profession)
 
 for(prof in professions){
   df_prof <- df_selected[df_selected$profession==prof,]
@@ -36,5 +41,23 @@ for(prof in professions){
   print(result)
 }
 
-"All professions failed to reject the null-hypothesis that the distribution is
-unimodal."
+"All professions failed to reject the null-hypothesis, so one cannot
+accept the alternative hypothesis that the distribution is at least bimodal."
+
+#----------------------------
+# TEST ACROSS SCORE LEVELS
+
+
+#Needs to round because some scores are not integer, because are averages.
+df_selected$round_qualification_score <- round(df_selected$qualification_score,digits = 0)
+scores <- unique(df_selected$round_qualification_score)
+
+for(score in scores){
+  df_score <- df_selected[df_selected$round_qualification_score==score,]
+  result <- dip.test(df_score$test_duration)
+  print(paste("Unimodality test for test_duration of score ",score))
+  print(result)
+}
+"Only score==0 rejected the null-hypothesis, which signifies that
+the has to accept that alternative hypothesis that distribution is 
+non-unimodal."
